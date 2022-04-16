9d2cf10c02d4a2754d21eb50622bbe0089655097
yuesha-yc <kevinwangyichen@gmail.com>
2022-04-16 15:44:22 -0700

    Data backend

--- backend/server.py
+++ backend/server.py
@@ -15,5 +15,20 @@ def index():
 def prediction():
     return {"percentage_seizure_occurrence": train.predict_label(2)}
 
+@app.route("/eeg/random")
+def random_data():
+    return {
+        "any": train.get_random_data(),
+        "seizure": train.get_random_seizure_data(),
+        "non_seizure": train.get_random_non_seizure_data()
+    }
+
+@app.route("/eeg/data")
+def all_data():
+    return {
+        "seizure": train.get_all_seizure_data(),
+        "non_seizure": train.get_all_non_seizure_data()
+    }
+
 if __name__ == "__main__":
     app.run(debug=True)
