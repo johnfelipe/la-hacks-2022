4664d3d1a81f0b1b00d48d8544c9e9224aed1787
yuesha-yc <kevinwangyichen@gmail.com>
2022-04-16 20:54:54 -0700

    WORKS

--- react-streaming-chart/src/App.js
+++ react-streaming-chart/src/App.js
@@ -1,53 +1,69 @@
-import React, { Component } from 'react';
-import { Line, Chart } from 'react-chartjs-2';
-import 'chartjs-adapter-luxon';
-import StreamingPlugin from 'chartjs-plugin-streaming';
-import './App.css';
+import React from "react";
+import { Line } from "react-chartjs-2";
+import Chart from "chart.js/auto";
+import { StreamingPlugin, RealTimeScale } from "chartjs-plugin-streaming";
+Chart.register(StreamingPlugin, RealTimeScale);
 
-Chart.register(StreamingPlugin);
+function App() {
+  const data = {
+    datasets: [
+      {
+        label: "Dataset 1",
 
-class App extends Component {
-  render() {
-    return (
-      <Line
-        data={{
-          datasets: [{
-            label: 'Dataset 1',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            borderColor: 'rgb(255, 99, 132)',
-            borderDash: [8, 4],
-            fill: true,
-            data: []
-          }, {
-            label: 'Dataset 2',
-            backgroundColor: 'rgba(54, 162, 235, 0.5)',
-            borderColor: 'rgb(54, 162, 235)',
-            cubicInterpolationMode: 'monotone',
-            fill: true,
-            data: []
-          }]
-        }}
-        options={{
-          scales: {
-            x: {
-              type: 'realtime',
-              realtime: {
-                delay: 2000,
-                onRefresh: chart => {
-                  chart.data.datasets.forEach(dataset => {
-                    dataset.data.push({
-                      x: Date.now(),
-                      y: Math.random()
-                    });
-                  });
-                }
-              }
-            }
-          }
-        }}
-      />
-    );
-  }
-}
+        fill: false,
+        lineTension: 0.4,
+        backgroundColor: "#f44336",
+        borderColor: "#f44336",
+        borderJoinStyle: "miter",
+        pointRadius: 0,
+        showLine: true,
+        data: [],
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      xAxes: [
+        {
+          type: "realtime",
+          realtime: {
+            onRefresh: function () {
+              data.datasets[0].data.push({
+                x: Date.now(),
+                y: Math.random() * 100,
+              });
+            },
+            delay: 300,
+            refresh: 300,
+          },
+        },
+      ],
+      yAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            fontFamily: "Arial",
+            labelString: "Moment",
+            fontSize: 20,
+            fontColor: "#6c757d",
+          },
+          ticks: {
+            max: 100,
+            min: 0,
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <div>
+      <div>
+        <Line data={data} options={options} width={400} height={200} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
