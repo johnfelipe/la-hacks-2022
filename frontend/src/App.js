480f0541c71cd674080dd64f018199a2e1541806
yuesha-yc <kevinwangyichen@gmail.com>
2022-04-17 07:41:16 -0700

    Percentage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,24 @@
-import React from "react";
-import { Line } from "react-chartjs-2";
-import Chart from "chart.js/auto";
-import { StreamingPlugin, RealTimeScale } from "chartjs-plugin-streaming";
-Chart.register(StreamingPlugin, RealTimeScale);
+import React, { useState, useEffect } from 'react'
 
 function App() {
 
-  const data = {
-    datasets: [
-      {
-        label: 'Dataset 1',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        borderColor: 'rgb(255, 99, 132)',
-        borderDash: [8, 4],
-        fill: true,
-        data: []
-      }
-    ]
-  };
+  const [data, setData] = useState([{}])
 
-  const onRefresh = chart => {
-    const now = Date.now();
-    chart.data.datasets.forEach(dataset => {
-      dataset.data.push({
-        x: now,
-        y: 100
-      });
-    });
-  };
-  
-  const options = {
-    scales: {
-      x: [{
-        type: 'realtime',
-        title: {
-          display: true,
-          text: 'Time'
-        },
-        realtime: {
-          duration: 20000,
-          refresh: 1000,
-          delay: 2000,
-          onRefresh: onRefresh
-        }
-      }],
-      y: [{
-        type: 'realtime',
-        title: {
-          display: true,
-          text: 'Value',
-        }
-      }]
-    }
-  }
+  useEffect(() => {
+    fetch("/prediction").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
 
   return (
-    <div>
-      <div>
-        <Line data={data} options={options} width={2000} height={1000} />
-      </div>
+    <div className="EEG">
+      <h1>Seizure Occurence Possibility: </h1>
+      <p>{data.percentage_seizure_occurrence}%</p>
     </div>
   );
 }
