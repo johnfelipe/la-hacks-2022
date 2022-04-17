874429154327372bdd7169499c9ed68fbdc6df0c
yuesha-yc <kevinwangyichen@gmail.com>
2022-04-16 19:34:48 -0700

    Line

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,113 @@
 import React, { useState, useEffect } from 'react'
+import {Chart} from 'chart.js';
+import 'chartjs-adapter-luxon';
+import ChartStreaming from "chartjs-plugin-streaming";
+
+import BarChart from './components/BarChart'
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+// import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => 100),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => 100),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
 function App() {
 
-  const [data, setData] = useState([{}])
+  Chart.register(ChartStreaming);
 
-  useEffect(() => {
-    fetch("/prediction").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
+    // Change default options for ALL charts
+  // Chart.defaults.set('plugins.streaming', {
+  //   duration: 20000
+  // });
 
-  return (
-    <div className="EEG">
-      <h1>Seizure Occurence Possibility: </h1>
-      <p>{data.percentage_seizure_occurrence}%</p>
-    </div>
-  );
+  // const chart = new Chart(ctx, {
+  //   options: {
+  //     plugins: {
+  //       // Change options for ALL axes of THIS CHART
+  //       streaming: {
+  //         duration: 20000
+  //       }
+  //     },
+  //     scales: {
+  //       x: {
+  //         type: 'realtime',
+  //         // Change options only for THIS AXIS
+  //         realtime: {
+  //           duration: 20000
+  //         }
+  //       }
+  //     }
+  //   }
+  // });
+
+  // const [data, setData] = useState([{}])
+
+  // useEffect(() => {
+  //   fetch("/prediction").then(
+  //     res => res.json()
+  //   ).then(
+  //     data => {
+  //       setData(data)
+  //       console.log(data)
+  //     }
+  //   )
+  // }, [])
+
+
+
+  
+  return <Line options={options} data={data} />;
+
 }
 
 export default App;
--- frontend/src/components/BarChart.js
+++ frontend/src/components/BarChart.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Bar } from "react-chartjs-2"
 
 const BarChart = () => {
-    return <div>
+    return (<div>
         <Bar
             data={{
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                }]
             }}
             height={400}
             width={600}
@@ -14,6 +18,7 @@ const BarChart = () => {
             }}
         />
     </div>
+    )
 }
 
 export default BarChart;
